eeea474a3d8203743cf66afbfabb20db7b4c9a76
sabiqaz <sabiqandazhar@gmail.com>
2021-11-26 22:01:36 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function index() {
+    const app = document.getElementById('app');
+    const h3 = document.createElement('h3');
+    h3.innerHTML = 'Prayer time';
+    app.appendChild(h3);
+}
+
+index();
