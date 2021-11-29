b13ea6c2e73414cd8f114c8f70bca1eacef68930
sabiqaz <sabiqandazhar@gmail.com>
2021-11-29 23:27:13 +0700

    permission geolocation

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('posisi tidak dapat diakses');
+}
+
+function userLoct() {
+    if(!navigator.geolocation){
+        alert('Geolocation tidak didukung perangkat anda, silahkan coba lagi');
+    } else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+
 function index() {
     const app = document.getElementById('app');
     const h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer time';
     app.appendChild(h3);
+
+    userLoct();
 }
 
 index();
